dffd1c20c5f0ade2cbdea9146826ef12731a0720
ericmagallan <ericmagallan85@gmail.com>
2019-04-18 01:12:01 -0700

    Pushing up the progress of the dashboard page. It's 75% complete with some minor styling issues, but nonetheless progress.

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,15 +1,90 @@
 import React, { Component } from 'react';
-import { View} from 'react-native';
+import { View, Image } from 'react-native';
 import DashHeaderCard from '../components/DashHeaderCard';
-import { Container, Header, Content, Card, CardItem, Text, Body } from "native-base";
+import { Container, Header, Button, Content, Card, CardItem, Text, Body } from 'native-base';
+import { ScrollView } from 'react-native-gesture-handler';
+import LinearGradient from 'react-native-linear-gradient';
+import { Actions } from 'react-native-router-flux';
+
 // import { Actions } from 'react-native-router-flux';
 
 export default class Dashboard extends Component {
+
+
     render() {
-      return (
-          <View>
-        <DashHeaderCard />
-        </View>
-      );
+        return (
+            <ScrollView >
+                <View>
+                    <LinearGradient
+                        colors={['#42AAD8', '#A8D7F7']}
+                        style={styles.container}>
+
+                        <Header />
+
+                        <View>
+                            <Text style={styles.redTex} onPress={() => Actions.main()}>go main page </Text>
+                            <Image source={require('../images/icebreakr-logo-icon.png')} style={{ alignSelf: 'center' }} />
+                            <Text style={{ color: 'white', textAlign: 'center', marginTop: 30, marginBottom: 30 }}>What do you want to do?</Text>
+                            <Card>
+                                <CardItem header>
+
+                                </CardItem>
+                                <Text style={{ color: 'black', textAlign: 'center', marginTop: 10, marginBottom: 10 }}>Private Message</Text>
+                                <Body>
+                                    <Text>
+                                    //Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                                    </Text>
+                                </Body>
+                                {/* </CardItem> */}
+
+                            </Card>
+
+                            <Card>
+                                <CardItem header>
+
+
+                                </CardItem>
+                                {/* <CardItem> */}
+                                <Body>
+                                    <Image source={require('../images/group-icon.png')} style={{ alignSelf: 'center' }} />
+
+                                    <Text style={{ textAlign: 'center', marginTop: 10, marginBottom: 10 }}>
+                                        Chat with a bunch of people. Answer questions and meet meme-able people like yourself
+                                        </Text>
+                                    
+
+                                </Body>
+                                <Button warning info style={styles.button}><Text style={{ color: 'white', textAlign: 'center', width: 300 }} onPress={() => Actions.signup()}>JOIN GROUP CHAT</Text></Button>
+                            </Card>
+
+                            <Card>
+                                <CardItem header>
+
+                                </CardItem>
+                                {/* <CardItem> */}
+                                <Body>
+                                    <Image source={require('../images/calendar-icon.png')} style={{ alignSelf: 'center' }} />
+                                    <Text style={{ textAlign: 'center', marginTop: 10, marginBottom: 10 }}>
+                                        Set up an event in your area. A social meetup or hangout with people.
+                                        </Text>
+                                    <Text style={{ color: 'black', textAlign: 'center', marginTop: 10, marginBottom: 10 }}>Set Up Event</Text>
+
+                                </Body>
+                                {/* </CardItem> */}
+
+                            </Card>
+                        </View>
+                    </LinearGradient>
+                </View>
+            </ScrollView>
+        );
     }
-  }
+}
+
+
+const styles = {
+    thisIsAStyle: {
+        fontSize: 20,
+        height: 25
+    },
+};
