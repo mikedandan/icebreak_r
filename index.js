fa433b98a10db11beee86a3450c8dc9c56ac97cf
Sam Wang <samuelwangkc@gmail.com>
2019-04-17 20:26:35 -0700

    restored routercomp on index.js

--- index.js
+++ index.js
@@ -2,9 +2,6 @@
 import React from 'react';
 import { AppRegistry, Text, View } from 'react-native';
 import RouterComp from './src/Router';
-import Boilerplate from './src/components/Boilerplate';
-import ThisPage from './src/pages/ThisPage';
-import GeoLoc from "./src/pages/test"
 import GroupChat from './src/pages/GroupChat';
 // Create a component 
 
@@ -12,8 +9,7 @@ import GroupChat from './src/pages/GroupChat';
 const App = () => {
     return (
 
-            // <GeoLoc />
-            <GroupChat />
+            <RouterComp />
     
     );
 };
